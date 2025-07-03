b79fbd2331f7bfc0a41be5b0bbe5aa9af6edce87
Seetha Ram Rao Vanam <Seetharam2602@gmail.com>
2025-07-03 14:04:40 -0500

    Initial Commit

---
+++ backend/src/main/java/com/jobtracker/jobapplicationstracker/JobApplicationsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jobtracker.jobapplicationstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationsTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jobtracker/jobapplicationstracker/JobApplicationsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobtracker.jobapplicationstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
